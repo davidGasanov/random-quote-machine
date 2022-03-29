ca750bf435e4a9ecbec61539be0634c07393d5ff
JohnnyVincent <tansacal@protonmail.com>
2022-03-29 12:16:14 +0500

    style: keep 'control' row in the main box after box size changes
    fix: fix the tweet button destination

--- src/App.js
+++ src/App.js
@@ -1,55 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-import QuoteCard from './Components/QuoteCard';
-import {useEffect, useState} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import QuoteCard from "./Components/QuoteCard";
+import { useEffect, useState } from "react";
 
 function App() {
+  const colorList = [
+    "#698996",
+    "#407076",
+    "#CE796B",
+    "#C18C5D",
+    "#06D6A0",
+    "#048A81",
+    "#8A89C0",
+    "#0A2E36",
+    "#14CC60",
+    "#A49694",
+    "#472836",
+  ];
 
-  const [currentQuote, updateQuote] = useState("")
-  const [currentAuthor, updateAuthor] = useState("")
-  const [cardHeight, changeHeight] = useState([80,120])
+  // useState
 
-  const fetchQuote = async () =>{
-    try{
+  const [currentQuote, updateQuote] = useState("");
+  const [currentAuthor, updateAuthor] = useState("");
 
-
-      const response = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch(
+        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+      );
       const data = await response.json();
       const quotes = data.quotes;
 
       const quoteIndex = Math.floor(Math.random() * quotes.length);
 
-      
-
       console.log(quotes);
 
       updateQuote(quotes[quoteIndex].quote);
-      updateAuthor(quotes[quoteIndex].author)
+      updateAuthor(quotes[quoteIndex].author);
       console.log(currentQuote + " " + currentAuthor);
-
-
-    } catch(error){
+    } catch (error) {
       console.log("an error has occured: " + error);
     }
-
-  }
+  };
 
   // provides the initial quote on each load
-  useEffect(()=>{
-
+  useEffect(() => {
     fetchQuote();
 
     //const textHeight = document.getElementById('text').offsetHeight;
     //console.log("TextHeight is:  " + textHeight);
-
-    
-
-  },
-[]);
+  }, []);
 
   return (
-    <div className="App">   
-      <QuoteCard  height={cardHeight} fetchQuote={fetchQuote} quote={currentQuote} author={currentAuthor}/>
+    <div className="App">
+      <QuoteCard
+        colorList={colorList}
+        fetchQuote={fetchQuote}
+        quote={currentQuote}
+        author={currentAuthor}
+      />
     </div>
   );
 }
--- src/Components/QuoteCard.js
+++ src/Components/QuoteCard.js
@@ -1,85 +1,86 @@
-import {AiOutlineTwitter} from 'react-icons/ai'
-import {useSpring, animated} from 'react-spring';
-import { useState, useEffect, useRef } from 'react';
-import Auto from '../auto.js';
+import { AiOutlineTwitter } from "react-icons/ai";
+import { useSpring, animated } from "react-spring";
+import { useState, useEffect, useRef } from "react";
 
-const QuoteCard = ({quote, author, fetchQuote, height}) => {
+const QuoteCard = ({ quote, author, fetchQuote, colorList }) => {
+  // --------- HOOKS
 
-  
-
-    // --------- HOOKS
-
-
-  // -- useEffect
-
+  // -- useState
   const [heightList, changeHeightList] = useState([]);
 
-  // -- useRef 
+  const [currentColor, changeCurrentColor] = useState(colorList[0]);
+
+  // -- useRef
 
   const myRef = useRef();
 
-  // -- useEffect 
+  // -- useEffect
 
-  useEffect(()=>{
-
+  useEffect(() => {
     // --- Get the current height of the p element
     const currentHeight = myRef.current.clientHeight;
-
-
-
     const heightListCopy = [...heightList];
 
+    const colorListCopy = [...colorList];
+    colorListCopy.filter((color) => color !== currentColor);
+
+    const colorId = Math.floor(Math.random() * colorListCopy.length);
+
+    changeCurrentColor(colorListCopy[colorId]);
+
     heightListCopy.push(currentHeight);
-
     changeHeightList(heightListCopy);
+  }, [quote]);
 
-    console.log("heightList is: " + heightList);
+  // -- React Spring
 
-    console.log("Current height is: "+currentHeight);
+  const previousHeight = heightList[heightList.length - 2] + 200;
+  const currentHeight = heightList[heightList.length - 1] + 200;
 
-  }
-  ,[quote])
+  console.log(previousHeight, currentHeight);
 
-
-  // -- React Spring 
-
-    const previousHeight = heightList[heightList.length-2]+200;
-    const currentHeight = heightList[heightList.length-1]+200;
-
-    console.log(previousHeight, currentHeight);
-
-    const styles = useSpring({
-
-      config: { duration: 500 },
-      native: true,
-      to : { height:`${currentHeight}px`, backgroundColor: 'green'},
-      from: {height: heightList.length>1 ? `${previousHeight}px` : `${currentHeight}px`, backgroundColor: 'red'},
-      reset: true,
-        })
-    
-
-  
-      
+  const styles = useSpring({
+    config: { duration: 500 },
+    native: true,
+    to: { height: `${currentHeight}px`, backgroundColor: currentColor },
+    from: {
+      height:
+        heightList.length > 1 ? `${previousHeight}px` : `${currentHeight}px`,
+    },
+    reset: true,
+  });
 
   return (
-  <animated.div id="quote-box" style={{
-    textAlign: 'center',
-    alignSelf: 'center',
-    padding: '30px',
-    backgroundColor: '#009FB7',
-    width: '500px',
-    color: 'white',
-    ...styles
-      }}>
-      <p ref={myRef} id="text">{quote}</p>
+    <animated.div
+      id="quote-box"
+      style={{
+        textAlign: "center",
+        alignSelf: "center",
+        padding: "30px",
+        backgroundColor: "#009FB7",
+        width: "500px",
+        color: "white",
+        ...styles,
+      }}
+    >
+      <p ref={myRef} id="text">
+        {quote}
+      </p>
       <p id="author">- {author}</p>
       <div id="control">
-          <button onClick={fetchQuote} id="new-quote">New quote</button>
-          <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote"><AiOutlineTwitter size={35} /></a>
+        <button onClick={fetchQuote} id="new-quote">
+          New quote
+        </button>
+        <a
+          href={`https://twitter.com/intent/tweet?text=${quote}`}
+          target="_blank"
+          id="tweet-quote"
+        >
+          <AiOutlineTwitter size={35} />
+        </a>
       </div>
-  </animated.div>
-
-    );
+    </animated.div>
+  );
 };
 
 export default QuoteCard;
